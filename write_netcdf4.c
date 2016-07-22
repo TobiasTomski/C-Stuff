4dc1cbd8737262c6f949227d6bdf390e757d7236
TobiasTomski <info@tobias-tomski.de>
2016-07-22 14:36:03 +0200

    Kinda initial netcdf4 commit. Initially wrote some code to create a netcdf4-file and print the id and the return value.

--- write_netcdf4.c
+++ write_netcdf4.c
@@ -1,2 +1,37 @@
 #include <stdlib.h>
-#include 
+#include <stdio.h>
+#include <netcdf.h>
+
+#define FILE_NAME "test.nc"
+
+#define NDIMS 4
+#define NT 24
+#define NL 1
+#define NX 21
+#define NY 21
+
+#define ERRCODE 2
+#define ERR(e) {printf("Error: %s\n", nc_strerror(e)); exit(ERRCODE);}
+
+int main()
+{
+    int ncid, t_dimid, l_dimid, x_dimid, y_dimid, varid;
+    int dimids[NDIMS];
+
+    int data[NT][NL][NY][NX];
+    int t, x, y, retval;
+
+    for (t = 0; t < NT; t++) {
+        for (x = 0; x < NX; x++) {
+            for (y = 0; y < NY; y++) {
+                data[t][0][x][y] = t + x + y;
+            }
+        }
+    }
+
+    if (retval = nc_create(FILE_NAME, NC_CLOBBER, &ncid)) {
+        ERR(retval);
+    }
+
+    printf("%d\n", retval, ncid);
+}
